49834f59d1213d49cb0a359722574e0cff1566e7
ideuxs <95435985+ideuxs@users.noreply.github.com>
2023-11-26 00:51:56 +0100

    Add files via upload

---
+++ ihm/Ihm.java
@@ -0,0 +1,59 @@
+package ihm;
+
+import structure.Terrain;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class Ihm {
+
+    private Terrain t;
+    public Ihm(Terrain t){
+        this.t = t;
+    }
+
+    public void saisie(){
+        Scanner sc = new Scanner(System.in);
+
+        String commande;
+        String [] mots;
+        while(!t.partieFinie()){
+            commande = sc.nextLine();
+            mots = commande.split("\\s+");
+            if(mots[0].equals("boardsize")){
+                if(mots.length == 2){
+                    int taille = Integer.parseInt(mots[1]);
+                    boolean check = t.boardSize(taille);
+                    System.out.println("=\n");
+                    if (!check){
+                        System.out.println("Dimensions incorrectes ou dimensions déjà saisies");
+                    }
+                }else{
+                    System.out.println("Argument manquant");
+                }
+            }else if(mots[0].equals("showboard")){
+                System.out.println("=\n");
+                t.showBoard();
+            } else if (mots[0].equals("play")) {
+                if(mots.length == 3){
+                    String name = mots[1];
+                    String param1 = mots[2];
+                    boolean check = t.placerPion(name, param1);
+                    if(!check){
+                        System.out.println("Argument manquant ou incorrect ou encore place déjà occupée.");
+                    }
+                }else {
+                    System.out.println("Argument manquant");
+                }
+            } else if(mots[0].equals("quit")){
+                exit(0);
+            }else{
+                System.out.println("? Unknown command");
+            }
+
+        }
+    }
+
+
+}
