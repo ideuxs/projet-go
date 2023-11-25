29aafe59144a9e6bd7755a1d40f7143039a09e79
ideuxs <95435985+ideuxs@users.noreply.github.com>
2023-11-26 00:47:45 +0100

    Update Joueur.java

--- joueur/Joueur.java
+++ joueur/Joueur.java
@@ -3,26 +3,56 @@ package joueur;
 public class Joueur {
     private boolean aPasser;
     private int nbPoints;
-    private char c;
+    private String c;
+    private String couleur;
 
     public Joueur(String couleur){
         aPasser = false;
         nbPoints = 0;
-        c = couleur.equals("blanc") ? 'O' : 'X';
+        this.couleur = couleur;
+
+        this.c = couleur.equals("blanc") ? "O" : "X";
     }
 
     public void setaPasser(){
         this.aPasser = true;
     }
 
+    public String getCouleur(){
+        return this.couleur;
+    }
+
+    public void placerPoint(String[][] t, int i, int j){
+        assert(t != null);
+        t[i][j] = c + "  ";
+    }
+    private boolean verifPlacement(String[][] t, int i, int j){
+        if(t[i-1][j] == null){
+            return false;
+        } else if (t[i][j-1]==null) {
+            return false;
+        } else if (t[i+1][j] == null) {
+            return false;
+        } else if (t[i][j+1] == null) {
+            return false;
+        }
+        return true;
+    }
     public void nePassePlus(){
         this.aPasser = false;
     }
-
     public void quite(){
-        String couleur = (c=='O' ? "blanc" : "noir");
+        String couleur = (this.couleur.equals("O") ? "blanc" : "noir");
         System.out.println("Abandon de la partie du joueur " + couleur);
         System.exit(0);
+    }
+
+    public void ajouterPoint(){
+        this.nbPoints += 1;
+    }
+
+    public String getChar(){
+        return c;
     }
 
     public boolean getaPasser() {
