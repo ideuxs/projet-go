443fba1fd541945e1ffe01659454237fc2ac811e
ideuxs <95435985+ideuxs@users.noreply.github.com>
2023-12-12 12:47:02 +0100

    Update Appli.java

--- appli/Appli.java
+++ appli/Appli.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 public class Appli {
     public static void main(String [] args){
         Terrain t = new Terrain();
-        Joueur j1 = new Joueur("noir");
-        Joueur j2 = new Joueur("blanc");
+        Joueur j1 = new Joueur("black");
+        Joueur j2 = new Joueur("white");
         t.ajouterJoueur(j1,j2);
         Ihm ihm = new Ihm(t);
         ihm.saisie();
-   }
+    }
 }
